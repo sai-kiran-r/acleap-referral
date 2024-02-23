4dced0fa21c1e9d92852f4d80b0511549c2ceaf3
sravandigitalhie <sravan@digitalhie.com>
2024-02-23 10:12:21 -0500

    Latest Update

--- src/components/ReferralStatusDialog/ReferralStatusDialog.tsx
+++ src/components/ReferralStatusDialog/ReferralStatusDialog.tsx
@@ -349,7 +349,7 @@ const ReferralStatusDialog = (props: ReferralStatusDialogProps) => {
                                     direction={sortDirection}
                                     onClick={() => handleRequestSort("noteAuthor")}
                                   ><b>
-                                    Author
+                                    Assigned Owner
                                   </b>
                                   </TableSortLabel>
                                 </TableCell>
