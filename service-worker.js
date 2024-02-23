842f05be1157da3c909e31260aab01e31bfb78d8
sravandigitalhie <sravan@digitalhie.com>
2024-02-23 10:19:41 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/acleap-referral/precache-manifest.acec0402a76d16d8f1798071ee482e0e.js"
+  "/acleap-referral/precache-manifest.b401bc60aec19610649b3017c9830276.js"
 );
 
 self.addEventListener('message', (event) => {
